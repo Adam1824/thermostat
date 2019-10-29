03124826ae7eaabf906907a19caf1bc806a45e89
Adam1824 <forrestadam07@gmail.com>
2019-10-29 16:29:16 +0000

    in progress of energy level

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -52,4 +52,34 @@ describe('Thermostat', function() {
       expect(thermostat.getCurrentTemperature()).toEqual(25);
     });
   });
+
+
+  describe('when power saving mode is off', function() {
+    it('has a maximum temperature of 32 degrees', function() {
+      thermostat.switchPowerSavingModeOff();
+      for (var i = 0; i < 13; i++) {
+        thermostat.up();
+      }
+      expect(thermostat.getCurrentTemperature()).toEqual(32);
+    });
+  });
+
+  it('can be reset to the default temperature', function() {
+    for (var i = 0; i < 6; i++) {
+      thermostat.up();
+    }
+    thermostat.resetTemperature();
+    expect(thermostat.getCurrentTemperature()).toEqual(20);
+  });
+
+  describe('displaying usage levels', function() {
+    describe('when the temperature is below 18 degrees', function() {
+      it('it is considered low-usage', function() {
+        for (var i = 0; i < 3; i++) {
+          thermostat.down()
+        }
+        expect(thermostat.energyUsage()).toEqual('low-usage');
+      });
+    });
+  });
 });
